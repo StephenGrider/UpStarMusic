f7821d847a07db8284ee6911878d1adc45027c14
stephen grider <ste.grider@gmail.com>
2016-11-02 09:53:42 -0700

    doc cleanup

--- database/FindArtist.js
+++ database/FindArtist.js
@@ -1,6 +1,5 @@
 /**
  * Finds a single artist in the artist collection.
- * @
  * @param {string} _id - The ID of the record to find.
  * @return {promise} A promise that resolves with the Artist that matches the id
  */
