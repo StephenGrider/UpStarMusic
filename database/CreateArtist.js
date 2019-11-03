f1fff48c611948530b188d2e045095ea337f3a18
stephen grider <ste.grider@gmail.com>
2019-11-03 10:12:33 -0700

    Added more comments

--- database/CreateArtist.js
+++ database/CreateArtist.js
@@ -1,2 +1,7 @@
+/**
+ * Finds a single artist in the artist collection.
+ * @param {object} artistProps - Object containing a name, age, yearsActive, and genre
+ * @return {promise} A promise that resolves with the Artist that was created
+ */
 export default () => {
 };
--- database/DeleteArtist.js
+++ database/DeleteArtist.js
@@ -1,2 +1,7 @@
+/**
+ * Deletes a single artist from the Artists collection
+ * @param {string} _id - The ID of the artist to delete.
+ * @return {promise} A promise that resolves when the record is deleted
+ */
 export default () => {
 };
